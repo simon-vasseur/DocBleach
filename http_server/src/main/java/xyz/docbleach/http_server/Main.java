ff818e98a040bac5d5cb386c8a03c2d8a6787957
Simon Vasseur <simon.vasseur@corp.ovh.com>
2017-05-02 15:15:23 +0000

    feat(api): check inside zip file

--- http_server/src/main/java/xyz/docbleach/http_server/Main.java
+++ http_server/src/main/java/xyz/docbleach/http_server/Main.java
@@ -2,9 +2,14 @@ package xyz.docbleach.http_server;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigException;
+import org.apache.tika.Tika;
 import xyz.docbleach.api.BleachSession;
 import xyz.docbleach.api.bleach.DefaultBleach;
 import xyz.docbleach.api.exception.BleachException;
@@ -15,6 +20,11 @@ import static spark.Spark.*;
 
 import java.io.*;
 import java.util.Base64;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
 
 public class Main {
 
@@ -22,6 +32,10 @@ public class Main {
 
     private static final int HTTP_BAD_REQUEST = 400;
     private static final int INTERNAL_SERVER_ERROR = 500;
+    
+    private static final Tika tika = new Tika();
+    private static final Config config = ConfigFactory.load();
+    private static final List<String> blackListedZipFileExtensions = config.getStringList("blackListedExtensions");
 
     interface Validable {
         boolean isValid();
@@ -29,19 +43,46 @@ public class Main {
 
     @Data
     static class PostPayload {
+        
+        @JsonProperty("FileName")
+        private String FileName;
+        
+        @JsonProperty("OriginalFileSize")
+        private Integer OriginalFileSize;
+        
+        @JsonProperty("Base64Size")
+        private Integer Base64Size;
+        
+        @JsonProperty("ContentType")
+        private String ContentType;
+        
+        @JsonProperty("MessageId")
+        private String MessageId;
+        
+        @JsonProperty("File")
         private String File;
         
         public boolean isValid() {
-            return File != null && !File.isEmpty();
+            return File != null && !File.isEmpty() && MessageId != null && !MessageId.isEmpty();
         }
     }
 
     @Data
     static class Sanitized {
+        
+        @JsonProperty("Status")
         private Boolean Status;
+        
+        @JsonProperty("Error")
         private String Error;
+        
+        @JsonProperty("File")
         private String File;
+        
+        @JsonProperty("OriginalFileSize")
         private int OriginalFileSize;
+        
+        @JsonProperty("Base64Size")
         private int Base64Size;
     }
 
@@ -49,19 +90,20 @@ public class Main {
 
         port(getPortNumber());
         
-        int maxThreads = 20;
-        int minThreads = 10;
+        int maxThreads = 50;
+        int minThreads = 20;
         int timeOutMillis = 60000;
         threadPool(maxThreads, minThreads, timeOutMillis);
 
-        post("/sanitize", (request, response) -> {
-            
+        post("/v1/synchronous/tasks", (request, response) -> {
+
             try {
                 PostPayload payload = getPayload(request.body());
                 if (!payload.isValid()) {
                     response.status(HTTP_BAD_REQUEST);
                     return "";
                 }
+                LOGGER.info(String.format("Processing %s", payload.getMessageId()));
                 Sanitized sanitized = sanitize(payload.getFile());
                 response.status(200);
                 response.type("application/json");
@@ -69,7 +111,7 @@ public class Main {
             } catch (JsonParseException | JsonMappingException ex) {
                 response.status(HTTP_BAD_REQUEST);
                 return "";
-            } catch (BleachException ex) {
+            } catch (BleachException | IOException ex) {
                 LOGGER.warn(String.format("DocBleach exception: %s", ex.toString()));
                 response.status(INTERNAL_SERVER_ERROR);
                 return "";
@@ -95,17 +137,23 @@ public class Main {
             JsonParseException, JsonMappingException {
                 
         ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
         PostPayload payload = mapper.readValue(body, PostPayload.class);
         return payload;
     }
     
-    private static Sanitized sanitize(String content) throws BleachException {
+    private static Sanitized sanitize(String content) throws BleachException, IOException {
         
         BleachSession session = new BleachSession(new DefaultBleach());
         byte[] decoded = Base64.getDecoder().decode(content);
 
         InputStream in = new ByteArrayInputStream(decoded);
+        String mimeType = tika.detect(in);
+
+        if (mimeType == "application/zip") {
+            LOGGER.info("Zip file detected");
+            in = purgeZipFile(in);
+        }
+
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         
         Sanitized sanitized = new Sanitized();
@@ -133,16 +181,62 @@ public class Main {
         
         return sanitized;
     }
-    
+
+    private static InputStream purgeZipFile(InputStream zipContent) throws IOException{
+            
+        ZipInputStream zis = new ZipInputStream(zipContent);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ZipOutputStream zos = new ZipOutputStream(bos);
+        byte[] buffer = new byte[1024];
+        
+        ZipEntry entry = zis.getNextEntry();
+
+        while(entry != null) {
+            String fileName = entry.getName();
+            String extension = getExtension(fileName);
+            
+            if(blackListedZipFileExtensions.contains(extension)){
+                LOGGER.info(String.format(
+                    "Removing %s from zip file -> blacklisted extension", 
+                    fileName
+                ));
+            } else {
+                ZipEntry newEntry = new ZipEntry(entry.getName());
+                zos.putNextEntry(newEntry);
+
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    zos.write(buffer, 0, len);
+                }
+                zos.closeEntry();
+            }
+            zis.closeEntry();
+            entry = zis.getNextEntry();
+        }
+        
+        zis.close();
+        zos.close();
+
+        return new ByteArrayInputStream(bos.toByteArray());
+    }
+
+    private static String getExtension(String fileName) {
+
+        String extension = "";
+
+        int i = fileName.lastIndexOf('.');
+        if (i > 0) {
+            extension = fileName.substring(i+1);
+        }
+        return extension.toLowerCase();
+    }
+
     private static int getPortNumber() {
         int port = 8080;
-        String PORT = System.getenv("PORT");
-        if (PORT != null && !PORT.isEmpty()) {
-            try {
-                port = Integer.valueOf(PORT);
-            } catch (NumberFormatException e) {
-                LOGGER.error("Invalid PORT defined in environment, falling back to 8080.");
-            }
+        try {
+            port = config.getInt("bindPort");
+        } catch (ConfigException e) {
+            LOGGER.error("Invalid PORT defined in environment, falling back to 8080.");
         }
         return port;
     }
